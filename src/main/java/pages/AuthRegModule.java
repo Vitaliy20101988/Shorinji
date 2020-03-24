ed484370c464d8054b7e80a236b4b533d5ef3d17
Vitaliy20101988 <zayath20@gmail.com>
2020-03-24 14:44:02 +0300

    ref

---
+++ src/main/java/pages/AuthRegModule.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class AuthRegModule extends BasePage {
+
+    public AuthRegModule(WebDriver driver){ super (driver, 10); }
+
+
+
+}
